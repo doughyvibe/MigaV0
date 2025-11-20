aa80feb866b1a8111807a5f6f224256e27adcdb4
Doughy <Doughyvibe@gmail.com>
2025-11-21 02:35:33 +0800

    Initial Commit

---
+++ MigaV0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MigaV0
+//
+//  Created by Doughy on 21/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MigaV0/MigaV0App.swift
@@ -0,0 +1,17 @@
+//
+//  MigaV0App.swift
+//  MigaV0
+//
+//  Created by Doughy on 21/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MigaV0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
